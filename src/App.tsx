162ee48734c96b92bc5af478e474bf0c2d508daf
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-12-24 14:06:36 +0700

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@chakra-ui/react"
+import { Button , ButtonGroup} from "@chakra-ui/react"
 
 function App() {
-  return <Button colorScheme='blue'>Click me</Button>;
+  return <Button colorScheme='black'>Click me</Button>;
 }
 
 export default App
