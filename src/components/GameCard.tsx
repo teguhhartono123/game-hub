5586e90e23bea77bb1742d2a072e582477fe4f8c
teguhhartono123 <teguhhartono123@yahoo.com>
2026-01-08 10:01:16 +0700

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import type { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
 import PlatformiconList from './PlatformiconList'
+import CriticScore from './CriticScore'
 
 interface Props{
     game: Game
@@ -12,7 +13,10 @@ const GameCard = ({game}: Props) => {
         <Image src={game.background_image}/>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
+            <HStack justifyContent='space-between'>
             <PlatformiconList platforms={game.parent_platforms.map(p => p.platform)}/>
+            <CriticScore score={game.metacritic}/>
+            </HStack>
         </CardBody>
     </Card.Root>
   )
