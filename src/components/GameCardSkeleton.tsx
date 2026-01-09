4a7f5a01aaa1c18df0b4f8975739f20f29c79297
teguhhartono123 <teguhhartono123@yahoo.com>
2026-01-09 21:18:57 +0700

    Show loading skeleton updated

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 const GameCardSkeleton = () => {
   return (
     <Card.Root borderRadius={10} overflow='hidden'>
-        <Skeleton height={'200px'}/>
+        <Skeleton width={'300px'}/>
         <CardBody>
             <SkeletonText/>
         </CardBody>
